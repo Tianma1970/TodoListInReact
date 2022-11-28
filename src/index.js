41e2c40f8e61fa1a2e38b7f00c9aa7d7c90f3df2
Tianma1970 <tillmann1970@gmail.com>
2022-11-28 14:37:39 +0100

    clean up code added root.render()

--- src/index.js
+++ src/index.js
@@ -16,8 +16,10 @@ root.render(
     <App />
   </React.StrictMode>
 )
-ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"))
+//console.log(DATA)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+root.render(<App tasks={DATA} />, document.getElementById("root"))
