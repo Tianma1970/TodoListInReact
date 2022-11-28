36279f5f8fb80316e675030a621c91008baa34bb
Tianma1970 <tillmann1970@gmail.com>
2022-11-28 15:27:05 +0100

    created a function iot temporarly alert the writen text

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Form(props) {
+  const [name, setName] = useState("")
+  function handleChange(e) {
+    setName(e.target.value)
+  }
   function handleSubmit(e) {
     e.preventDefault()
-    props.addTask("Hello World")
+    props.addTask(name)
+    setName("")
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -12,7 +17,7 @@ function Form(props) {
           What needs to be done?
         </label>
       </h2>
-      <input type="text" id="new-todo-input" className="input input__lg" name="text" autoComplete="off" />
+      <input type="text" id="new-todo-input" className="input input__lg" name="text" autoComplete="off" value={name} onChange={handleChange} />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
       </button>
