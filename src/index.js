1feae185cdc7242b1779bfb95a549f99f20afb8f
Tianma1970 <tillmann1970@gmail.com>
2022-11-29 13:37:33 +0100

    removed unused code

--- src/index.js
+++ src/index.js
@@ -3,20 +3,9 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 
-const DATA = [
-  { id: "todo-0", name: "Eat", completed: "true" },
-  { id: "todo-1", name: "Sleep", completed: "false" },
-  { id: "todo-2", name: "Repeat", completed: "false" }
-]
+const DATA = []
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-//console.log(DATA)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
