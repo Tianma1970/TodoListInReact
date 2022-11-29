fe735560664421d2dd0320f819a7f972324f4ba0
Tianma1970 <tillmann1970@gmail.com>
2022-11-29 10:16:05 +0100

    make the remaining tasks dynamic

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App(props) {
     setTasks([...tasks, newTask])
   }
   const taskList = tasks.map(task => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />)
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task"
+  const headingText = `${taskList.length} ${tasksNoun} tasks remaining`
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -21,7 +23,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
         {taskList}
       </ul>
